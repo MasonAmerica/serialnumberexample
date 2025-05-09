d02e0a4b4ffd7b8e35645c39e4b19537ab65c254
Cody Brookshear <codybrookshear@bymason.com>
2025-05-08 23:30:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/dualmasonsdkexample/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.dualmasonsdkexample
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dualmasonsdkexample.ui.theme.DualMasonSDKExampleTheme
+import masonamerica.platform.DeviceIdentifiers
+import masonamerica.platform.MasonFramework
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var ser = "UNKNOWN"
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.O) {
+            ser = android.os.Build.SERIAL
+        } else {
+            try {
+                // This will only work if deployed to a device via a Mason Controller build.
+                var di = MasonFramework.get(baseContext, DeviceIdentifiers::class.java)
+                ser = di.serial
+            } catch (e: Exception) {
+                // ser is already set to UNKNOWN
+            }
+        }
+
+        enableEdgeToEdge()
+        setContent {
+            DualMasonSDKExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        serial = ser,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(serial: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Serial: $serial",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DualMasonSDKExampleTheme {
+        Greeting("Android")
+    }
+}
